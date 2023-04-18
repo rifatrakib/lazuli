8a939ca144d43d84138676f65082e577d1cb4ac7
rifatrakib <Rakib.1508@outlook.com>
2023-04-18 14:54:55 +0600

    scrape reviews when available and merge

--- adidas/spiders/products.py
+++ adidas/spiders/products.py
@@ -2,6 +2,8 @@ import json
 from urllib.parse import parse_qs, urlparse
 
 import scrapy
+from scrapy.selector import Selector
+from w3lib.html import remove_tags
 
 
 class ProductsSpider(scrapy.Spider):
@@ -21,22 +23,19 @@ class ProductsSpider(scrapy.Spider):
     def parse_links(self, response):
         data = response.json()
 
-        # if "canonical_param_next" in data:
-        #     endpoint = data["canonical_param_next"].replace("item/", "list")
-        #     yield scrapy.Request(
-        #         f"{self.catalogue_url_base}/{endpoint}",
-        #         callback=self.parse_links,
-        #     )
+        if "canonical_param_next" in data:
+            endpoint = data["canonical_param_next"].replace("item/", "list")
+            yield scrapy.Request(
+                f"{self.catalogue_url_base}/{endpoint}",
+                callback=self.parse_links,
+            )
 
         for product_code, information in data["articles"].items():
-            if product_code != "HF1845":
-                continue
             yield scrapy.Request(
                 f"{self.product_page_base}/{product_code}/",
                 callback=self.parse_product_page,
                 cb_kwargs=information,
             )
-            break
 
     def parse_product_page(self, response, **kwargs):
         product_page_url = response.url
@@ -46,7 +45,9 @@ class ProductsSpider(scrapy.Spider):
         sizes = response.css("div.test-sizeSelector > ul button::text").getall()
         sense_of_fit = "適切" if response.css("span.test-marker").get() else None
         title_of_description = response.css("h4.itemFeature::text").get()
-        itemization_description = response.css("li.articleFeaturesItem").getall()
+        itemization_description = []
+        for item in response.css("li.articleFeaturesItem").getall():
+            itemization_description.append(remove_tags(item))
 
         data = {
             "url": product_page_url,
@@ -77,37 +78,88 @@ class ProductsSpider(scrapy.Spider):
         data = response.json()
 
         if kwargs["product_stat"]["review_count"] > 0:
-            model_code = kwargs["product_stat"]["article"]
-            params = f"format=embeddedhtml&productattribute_itemKcod={model_code}&scrollToTop=true"
+            item = kwargs["product_stat"]["article"]
+            model_code = kwargs["product_stat"]["model_code"]
+            params = f"format=embeddedhtml&productattribute_itemKcod={item}&scrollToTop=true"
             yield scrapy.Request(
-                f"{self.reviews_url_base}?{params}",
+                f"{self.reviews_url_base.replace('<model>', model_code)}?{params}",
                 callback=self.parse_reviews,
                 cb_kwargs={**kwargs, "size_chart": data},
             )
+        else:
+            yield {**kwargs, "size_chart": data, "review_data": {}}
 
     def parse_reviews(self, response, **kwargs):
-        print(response.status, response.url)
-        reviews = []
+        review_data = {} if "review_data" not in kwargs else kwargs["review_data"]
+        reviews = [] if "reviews" not in review_data else review_data["reviews"]
 
         for line in response.body.decode().split("\n"):
             if line.startswith("var materials="):
-                data = line.replace("var materials=", "")[:-1]
-                print(json.loads(data).keys())
-                break
+                review_info = json.loads(line.replace("var materials=", "")[:-1])
+                review_html = Selector(text=review_info["BVRRSourceID"].replace("\\", ""), type="html")
 
         total_page = kwargs["product_stat"]["review_count"] // 10 + 1
         parsed_url = urlparse(response.url)
         query_params = parse_qs(parsed_url.query)
         next_page = int(query_params.get("page", ["1"])[0]) + 1
 
+        if next_page == 2:
+            rating = review_html.css(
+                "#BVRRRatingOverall_ > div.BVRRRatingNormalOutOf > span.BVRRNumber.BVRRRatingNumber::text"
+            ).get()
+            number_of_reviews = review_html.css("span.BVRRNumber.BVRRBuyAgainTotal::text").get()
+            recommended_rate = review_html.css("span.BVRRBuyAgainPercentage > span.BVRRNumber::text").get()
+            sense_of_fit_rate = review_html.css(
+                "div.BVRRSecondaryRatingsContainer div.BVRRRatingFit img::attr(title)"
+            ).get()
+            appropriation_of_length_rate = review_html.css(
+                "div.BVRRSecondaryRatingsContainer div.BVRRRatingLength img::attr(title)"
+            ).get()
+            material_quality_rate = review_html.css(
+                "div.BVRRSecondaryRatingsContainer div.BVRRRatingQuality img::attr(title)"
+            ).get()
+            comfort_rate = review_html.css(
+                "div.BVRRSecondaryRatingsContainer div.BVRRRatingComfort img::attr(title)"
+            ).get()
+
+            review_data = {
+                "rating": rating,
+                "number_of_reviews": number_of_reviews,
+                "recommended_rate": recommended_rate,
+                "sense_of_fit_rate": sense_of_fit_rate,
+                "appropriation_of_length_rate": appropriation_of_length_rate,
+                "material_quality_rate": material_quality_rate,
+                "comfort_rate": comfort_rate,
+            }
+
+        for review_section in review_html.css("#BVSubmissionPopupContainer"):
+            review_date = review_section.css("span.BVRRReviewDate::text").get()
+            review_rating = review_section.css(
+                "#BVRRRatingOverall_Review_Display > div.BVRRRatingNormalImage > img::attr(title)"
+            ).get()
+            review_title = review_section.css("span.BVRRReviewTitle::text").get()
+            review_description = review_section.css("span.BVRRReviewText::text").get()
+            reviewer_id = review_section.css("span.BVRRNickname::text").get()
+            reviews.append(
+                {
+                    "review_date": review_date,
+                    "review_rating": review_rating,
+                    "review_title": review_title,
+                    "review_description": review_description,
+                    "reviewer_id": reviewer_id,
+                }
+            )
+
+        review_data["reviews"] = reviews
         if next_page <= total_page:
-            model_code = kwargs["product_stat"]["article"]
-            params = f"format=embeddedhtml&page={next_page}&productattribute_itemKcod={model_code}&scrollToTop=true"
+            item = kwargs["product_stat"]["article"]
+            model_code = kwargs["product_stat"]["model_code"]
+            params = f"format=embeddedhtml&page={next_page}&productattribute_itemKcod={item}&scrollToTop=true"
 
             yield scrapy.Request(
-                f"{self.reviews_url_base}?{params}",
+                f"{self.reviews_url_base.replace('<model>', model_code)}?{params}",
                 callback=self.parse_reviews,
-                cb_kwargs=kwargs,
+                cb_kwargs={**kwargs, "review_data": review_data},
             )
         else:
-            yield {**kwargs, "reviews": reviews}
+            yield {**kwargs, "review_data": review_data}
