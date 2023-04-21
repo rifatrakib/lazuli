ad473a7998e0c8792bd2f86ed2a8ff942e99558a
rifatrakib <Rakib.1508@outlook.com>
2023-04-21 12:51:11 +0600

    integrate middlewares to store metadata

--- adidas/settings.py
+++ adidas/settings.py
@@ -37,15 +37,15 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-# SPIDER_MIDDLEWARES = {
-#    "adidas.middlewares.AdidasSpiderMiddleware": 543,
-# }
+SPIDER_MIDDLEWARES = {
+    "adidas.middlewares.AdidasSpiderMiddleware": 543,
+}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#    "adidas.middlewares.AdidasDownloaderMiddleware": 543,
-# }
+DOWNLOADER_MIDDLEWARES = {
+    "adidas.middlewares.AdidasDownloaderMiddleware": 543,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
